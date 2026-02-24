8c2207fb24b33434364a2f02783a6860e8a3de07
Lance Armstrong <your_email@example.com>
2026-02-24 10:19:29 +0800

    fixing home.html

--- peo_system/main/urls.py
+++ peo_system/main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("admin-division/dashboard/", views.admin_div_dashboard, name="admin_div_dashboard"),
     path("Admin/adminDiv.html", views.admin_div_dashboard, name="admin_div_dashboard_legacy"),
 ]
--- peo_system/peo_system/urls.py
+++ peo_system/peo_system/urls.py
@@ -1,13 +1,10 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.contrib.auth import views as auth_views
-
-from main.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
+    path('', include('main.urls')),
     path(
         'accounts/login/',
         auth_views.LoginView.as_view(
@@ -17,13 +14,4 @@ urlpatterns = [
         name='login',
     ),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', home, name='home'),
-=======
-    path('', include('main.urls')),
-
-    path ('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path ('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-
-    path ('',home, name='home'),
->>>>>>> d09195ab9d34895369dfe9ffb3602105326bbe5f
 ]
